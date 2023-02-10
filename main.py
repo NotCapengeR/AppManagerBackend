35d251cf3a5a27092d220391fbdd65f2041ffb69
NotCapengeR <purple.ka4ok@gmail.com>
2023-02-10 22:51:25 +0500

    Removed reduntat dependencies & added auto create tables

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 # -*- coding: UTF-8 -*-
 from typing import NoReturn
 
-from src import app, api, config
+from src import app, api, config, db
 from src.api.routes import Login, Register, BackupManager, BackupProvider, DownloadBackup
 
 
 def main() -> NoReturn:
+    with app.app_context():
+        db.create_all()
     api.add_resource(Login, Login.url)
     api.add_resource(Register, Register.url)
     api.add_resource(BackupManager, BackupManager.url)
